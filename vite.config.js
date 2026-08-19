c705b573a42d6603683bd035bbecb9f48d16144f
Sokun-168 <gkun9996@gmail.com>
2026-08-19 22:19:29 +0700

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ETEC-FINAL-PROJECT/',
+  base: '/ETEC-Final-Project/',
   plugins: [react(), tailwindcss()],
 })
